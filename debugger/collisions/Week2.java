71bf5e6c89d7bca7e18620a8d9f587b5eec97151
maryregan <78108025+maryregan@users.noreply.github.com>
2022-10-02 22:16:31 -0400

    debugger week 2 done

--- debugger/collisions/Week2.java
+++ debugger/collisions/Week2.java
@@ -12,17 +12,24 @@ public final class Week2 extends Week2Reqs {
 	
 	@Override
 	public boolean isColliding(AABShape s1, AABShape s2) {
-		return false;
+		boolean projX = (s1.getTopLeft().x <= s2.getTopLeft().x+s2.getSize().x) && (s1.getTopLeft().x+s1.size.x >= s2.getTopLeft().x);
+		boolean projY = (s1.getTopLeft().y <= s2.getTopLeft().y+s2.getSize().y) && (s1.getTopLeft().y+s1.size.y >= s2.getTopLeft().y);
+		return projX && projY;
 	}
 
 	@Override
 	public boolean isColliding(AABShape s1, CircleShape s2) {
-		return false;
+		Vec2d maxes = new Vec2d(s1.getTopLeft().x + s1.getSize().x, s1.getTopLeft().y + s1.getSize().y);
+		Vec2d nearestPoint = this.clamp(s2.getCenter(), s1.getTopLeft(), maxes);
+		double dist = s2.getCenter().dist2(nearestPoint);
+		return dist < (s2.getRadius() * s2.getRadius());
 	}
 
 	@Override
 	public boolean isColliding(AABShape s1, Vec2d s2) {
-		return false;
+		boolean inX = s1.getTopLeft().x <= s2.x && s1.getTopLeft().x+s1.getSize().x >= s2.x;
+		boolean inY = s1.getTopLeft().y <= s2.y && s1.getTopLeft().y+s1.getSize().y >= s2.y;
+		return inX && inY;
 	}
 
 	// CIRCLES
@@ -34,13 +41,22 @@ public final class Week2 extends Week2Reqs {
 
 	@Override
 	public boolean isColliding(CircleShape s1, CircleShape s2) {
-		return false;
+		double distCenters = s1.getCenter().dist2(s2.getCenter());
+		double dif = s1.getRadius() + s2.getRadius();
+		double distRadius = dif*dif;
+		return distCenters <= distRadius;
 	}
 
 	@Override
 	public boolean isColliding(CircleShape s1, Vec2d s2) {
-		return false;
+		double dist = s1.getCenter().dist2(s2);
+		return dist < (s1.getRadius() * s1.getRadius());
 	}
 
+	private Vec2d clamp(Vec2d value, Vec2d mins, Vec2d maxes){
+		double xClamp = Math.max(mins.x, Math.min(maxes.x, value.x));
+		double yClamp = Math.max(mins.y, Math.min(maxes.y, value.y));
+		return new Vec2d(xClamp, yClamp);
+	}
 	
 }
